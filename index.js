0716aaff14d2750b24e755477c5360b4edf4024a
danmartin98 <danmartin98@icloud.com>
2021-12-06 20:29:39 +0000

    all tasks done, not bonus ones though

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
-let myCollection = [
-  {
-    name: 'School of Code mug',
-    count: 1,
-    whatILike: 'It has my cute pixel character on it!',
-  },
-  {
-    name: 'School of Code hat',
-    count: 2,
-    whatILike: 'An often overlooked fashion accessory',
-  },
-  {
-    name: 'School of Code pillow',
-    count: 1,
-    whatILike: 'Eat. Sleep. Code. Repeat :)',
-  },
-];
+import myCollection from "./collection.js";
+import chalk from "chalk";
+
+console.log(chalk.blue('Hello world!'));
 
 function describeItem(item) {
+  if (item.count > 1) {
+    console.log(
+      'I have ' + chalk.yellow(item.count) + " " +
+        chalk.cyan(item.name) + "'s" +
+        ". Here's what I like about it: " +
+        chalk.green(item.whatILike)
+    );
+  } else 
   console.log(
-    'I have a name. ' +
-      item.name +
-      " Here's what I like about it: " +
-      item.whatILike
+    'I have ' + chalk.yellow(item.count) + " " +
+      chalk.cyan(item.name) +
+      ". Here's what I like about it: " +
+      chalk.green(item.whatILike)
   );
 }
 
