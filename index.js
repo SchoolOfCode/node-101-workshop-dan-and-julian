3d81d8b7a4b0ddbf77750b7d7843c771a318eaa8
danmartin98 <danmartin98@icloud.com>
2021-12-06 16:28:06 +0000

    first task

---
+++ index.js
@@ -0,0 +1,23 @@
+let myCollection = [
+    {
+      name: "School of Code mug",
+      count: 1,
+      whatILike: "It has my cute pixel character on it!"
+    },
+    {
+      name: "School of Code hat",
+      count: 2,
+      whatILike: "An often overlooked fashion accessory"
+    },
+    {
+      name: "School of Code pillow",
+      count: 1,
+      whatILike: "Eat. Sleep. Code. Repeat :)"
+    }
+  ];
+
+  function describeItem(item) {
+      console.log(item.name)
+  }
+
+  describeItem(myCollection[0])
